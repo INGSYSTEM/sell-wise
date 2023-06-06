2a6842397e6a4497ba50c1d380a1e06a6d3c2130
INGSYSTEM <juanromerocollazos@gmail.com>
2023-06-05 23:45:17 -0500

    se realizo actualización en el repositorio

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
   <div class="contenido-principal">
     <div class="contenedor-1">
       <img src="img/logo_sellwise.png" alt="SellWise">
-      <p class="textos-cortos">Optimiza y gestiona tus operaciones comerciales</p>
+      <p class="textos-frases">Optimiza y gestiona tus operaciones comerciales</p>
       <button>Comienza Ahora</button>
     </div>
   </div>
