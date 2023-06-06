da261367c62480de9a56f7ea29def7a442b2ee0b
INGSYSTEM <juanromerocollazos@gmail.com>
2023-06-05 23:07:06 -0500

    se realizo actualización en el repositorio

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/global.css">
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+  <title>SellWise</title>
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <img src="img/logo_sellwise.png" alt="SellWise">
+    </div>
+    <div class="menu">
+      <ul>
+        <li><a href="#">Soluciones</a></li>
+        <li><a href="#">Planes</a></li>
+        <li><a href="#">Iniciar Sesión</a></li>
+      </ul>
+    </div>
+  </header>
+  <div class="contenido-principal">
+    <div class="contenedor-1">
+      <img src="img/logo_sellwise.png" alt="SellWise">
+      <p>Optimiza y gestiona tus operaciones comerciales</p>
+      <button>Comienza Ahora</button>
+    </div>
+  </div>
+</body>
+</html>
